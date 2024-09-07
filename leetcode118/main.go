67606df28b9fc0fd32f1238756d5be7166c7ca4c
manendra99 <manendra16200singh@gmail.com>
2024-09-07 22:04:10 +0530

    learn : leetcode 118 pascal triangle

--- leetcode118/main.go
+++ leetcode118/main.go
@@ -7,6 +7,12 @@ import (
 // Input: numRows = 5
 // Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
+//Approach represents
+//1 -> create a first row if the output 2d array had length = 0
+//2 -> make a empty [] with length = col number and assign 1 and last value to 1
+//3-> loop through the new row and check where values are 0 and assigned those indices with the value for previousRow[sameIndex  ] + previousRow[sameIndex -1 ]
+//4 -> append the new row to the output
+
 func main() {
 
 	numberOfRows := 5
@@ -21,8 +27,6 @@ func main() {
 
 			newRow := make([]int, i+1)
 			for j, _ := range outputMatrix[i-1] {
-
-				// numberOfCols := i + 1
 
 				if j == 0 {
 
